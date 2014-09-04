cf5b8a701c61be46e3a7de18de015a7d7cad1a23
Nobu19800 <TyouKyozyakuTaisitu@yahoo.co.jp>
2014-09-05 07:02:11 +0900

    702

--- OOoWriterRTC.py
+++ OOoWriterRTC.py
@@ -12,9 +12,9 @@ import os.path
 
 
 if os.name == 'posix':
-    sys.path += ['/usr/lib/openoffice/basis-link/program/WriterIDL', '/usr/lib/python2.6/dist-packages', '/usr/lib/python2.6/dist-packages/rtctree/rtmidl']
+    sys.path += ['./OOoRTC', './OOoRTC/WriterIDL', '/usr/lib/python2.6/dist-packages', '/usr/lib/python2.6/dist-packages/rtctree/rtmidl']
 elif os.name == 'nt':
-    sys.path += ['.\\WriterIDL', 'C:\\Python26\\lib\\site-packages', 'C:\\Python26\\lib\\site-packages\\rtctree\\rtmidl']
+    sys.path += ['.\\OOoRTC', '.\\OOoRTC\\WriterIDL', 'C:\\Python26\\lib\\site-packages', 'C:\\Python26\\lib\\site-packages\\rtctree\\rtmidl']
     
 
 
